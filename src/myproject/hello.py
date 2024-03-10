04f96f7ccf95959ca245bf416005d72abd72b034
Matthias Dittrich <matthi.d@gmail.com>
2024-03-10 12:51:29 +0100

    Initial project setup

---
+++ src/myproject/hello.py
@@ -0,0 +1,7 @@
+# src/myproject/hello.py
+
+def greet():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    greet()
---
+++ src/setup.py
@@ -0,0 +1,8 @@
+# src/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="myproject",
+    version="0.1",
+    packages=find_packages(),
+)
