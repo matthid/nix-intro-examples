fac803fdcfe04eae4e71d7576cf02c6b2d882169
Matthias Dittrich <matthi.d@gmail.com>
2024-03-10 13:32:34 +0100

    Update nix/shell.nix to include myProject as a build input

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
-
+let
+  myProject = import ./project.nix { pkgs = pkgs; };
+in
 pkgs.mkShell {
   buildInputs = [
     pkgs.python3
+    myProject
     pkgs.git
     pkgs.curl
   ];
